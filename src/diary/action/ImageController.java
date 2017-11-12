31e62453cb4069c593c5ce9f3053f95357d87876
liangxu <MSI@DESKTOP-KLINSMP>
2017-11-12 11:15:16 +0800

    ultrasonic

---
+++ src/diary/action/ImageController.java
@@ -0,0 +1,12 @@
+package diary.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by MSI on 2017/11/12.
+ */
+@Controller
+@RequestMapping("/image")
+public class ImageController {
+}
