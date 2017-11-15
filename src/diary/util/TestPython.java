fccda5c7457f9f428aa9c7575fd02b041383215f
liangxu <MSI@DESKTOP-KLINSMP>
2017-11-15 19:26:14 +0800

    matlab

--- src/diary/util/TestPython.java
+++ src/diary/util/TestPython.java
@@ -1,30 +1,57 @@
 package diary.util;
 
-import org.python.core.*;
-import org.python.util.PythonInterpreter;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import MY_ULTRASONIC.Ultrasonic;
+import com.mathworks.toolbox.javabuilder.*;
 
 /**
  * Created by MSI on 2017/11/4.
  */
 public class TestPython {
-    public static void main(String args[])
-    {
-        String shell = "cmd /c start E:\\sota\\Diary\\FolderListener.exe";//需要执行的命令
-//        String arg="d:\\fusheng\\pics";
-//        String cmd="python";
-//        String file="d:\\fusheng\\test\\deploy_interface.py";
-//        String[] shell=new String[]{
-//                cmd,file,arg
-//        };
-        try {
-            Process p = Runtime.getRuntime().exec(shell);//调用控制台执行shell
-        } catch (Exception e) {
+    public static void main(String[] args) throws Exception{
+// TODO Auto-generated method stub
+        MWNumericArray a = null;MWNumericArray m = null;MWNumericArray n = null;MWNumericArray p = null;MWNumericArray q = null;
+        MWNumericArray b = null;
+        MWCharArray c = null;
+        Object[] result= null;
+        Ultrasonic ultrasonic=null;
+        try{
+            double[] aaa={164,203,203,250};
+            double[] bbb={508,472,573,519};
+
+            a = new MWNumericArray(aaa,MWClassID.DOUBLE);
+            b = new MWNumericArray(bbb, MWClassID.DOUBLE);
+            c = new MWCharArray("d:\\fusheng\\GUI\\PICimg\\002");
+
+            m= new MWNumericArray(Double.valueOf("518"),MWClassID.DOUBLE);
+            n=new MWNumericArray(Double.valueOf("620"),MWClassID.DOUBLE);
+            p=new MWNumericArray(Double.valueOf("94"),MWClassID.DOUBLE);
+            q=new MWNumericArray(Double.valueOf("133"),MWClassID.DOUBLE);
+            c=new MWCharArray("d:\\fusheng\\GUI\\PICimg\\002");
+            ultrasonic=new Ultrasonic();
+            result=ultrasonic.MY_ULTRASONIC(3,c,a,b,m,n,p,q);
+            System.out.println(result[0]);
+            System.out.println(result[1]);
+            System.out.println(result[2]);
+
+        }catch (Exception e) {
+// TODO: handle exception
+            System.out.println("Exception! "+e.toString());
+            e.printStackTrace();
+        }finally{
+//free native resoures
+            MWArray.disposeArray(a);
+            MWArray.disposeArray(b);
+            MWArray.disposeArray(c);
+            MWArray.disposeArray(m);
+            MWArray.disposeArray(n);
+            MWArray.disposeArray(p);
+            MWArray.disposeArray(q);
+
+            MWArray.disposeArray(result);
+            ultrasonic.dispose();
         }
+
     }
 }//main
 
